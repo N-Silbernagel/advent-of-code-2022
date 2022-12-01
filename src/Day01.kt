3cba52979b37af46680ccd2fb221546294a2f144
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2022-12-01 22:24:01 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,49 @@
+import java.util.*
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    var mostCalories = 0
+    var currentCalories = 0
+
+    for (line in input) {
+        if (line.isBlank()) {
+            if (currentCalories > mostCalories) {
+                mostCalories = currentCalories
+            }
+            currentCalories = 0
+            continue
+        }
+
+        val lineCalories = line.toInt()
+        currentCalories += lineCalories
+    }
+
+    return mostCalories
+}
+
+fun part2(input: List<String>): Int {
+    val top3Calories = TreeSet<Int>()
+
+    var currentCalories = 0
+    for (line in input) {
+        if (line.isBlank()) {
+            top3Calories.add(currentCalories)
+            if (top3Calories.size > 3) {
+                top3Calories.remove(top3Calories.first())
+            }
+
+            currentCalories = 0
+            continue
+        }
+
+        val lineCalories = line.toInt()
+        currentCalories += lineCalories
+    }
+
+    return top3Calories.sum()
 }
