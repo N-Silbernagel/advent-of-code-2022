7c97cfc3b2f420c531ae1e6401b623a15411b9b4
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2022-12-06 17:40:15 +0100

    day06

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,55 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val stream = input[0]
+
+        val buffer = ArrayList<Char>()
+
+        for ((index, char) in stream.withIndex()) {
+            buffer.add(char)
+
+            if (index < 3) {
+                continue
+            }
+
+            val uniqueChars = HashSet<Char>()
+            uniqueChars.addAll(buffer)
+            if (uniqueChars.size == 4) {
+                return index + 1
+            }
+
+            buffer.remove(buffer.first())
+        }
+
+        return -1
+    }
+
+    fun part2(input: List<String>): Int {
+        val stream = input[0]
+
+        val buffer = ArrayList<Char>()
+
+        for ((index, char) in stream.withIndex()) {
+            buffer.add(char)
+
+            if (index < 13) {
+                continue
+            }
+
+            val uniqueChars = HashSet<Char>()
+            uniqueChars.addAll(buffer)
+            if (uniqueChars.size == 14) {
+                return index + 1
+            }
+
+            buffer.remove(buffer.first())
+        }
+
+        return -1
+
+    }
+
+    val input = readInput("Day06")
+    println(part1(input))
+    println(part2(input))
+
+}
